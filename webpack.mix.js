62b10b6d5ffcf03a1837ab86414108e647340b67
Masoud Reyhanian <mrhn2005@gmail.com>
2021-03-14 16:34:59 +0330

    template

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,31 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+ mix.styles([
+    'resources/template/css/bootstrap.css',
+    'resources/template/css/revolution-slider.css',
+    'resources/template/css/style.css',
+    'resources/template/css/responsive.css',
+], 'public/template/main.css');
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js([
+    'resources/template/js/jquery.js',
+    'resources/template/js/bootstrap.min.js',
+    // 'resources/template/js/custom.scrollbar.js',
+    // 'resources/template/js/revolution.min.js',
+    'resources/template/js/jquery.fancybox.pack.js',
+    'resources/template/js/jquery.fancybox-media.js',
+    // 'resources/template/js/owl.js',
+    // 'resources/template/js/wow.js',
+    // 'resources/template/js/appear.js',
+    // 'resources/template/js/isotope.js',
+    // 'resources/template/js/mixitup.js',
+    'resources/template/js/script.js',
+], 'public/template/main.js');
+
+mix.copyDirectory('resources/template/fonts', 'public/template/fonts');
+
+mix.copyDirectory('resources/template/images', 'public/images');
+
+mix.copyDirectory('resources/template/css/assets', 'public/template/css/assets');
+
