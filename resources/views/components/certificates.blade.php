b4c9d36ab0c78f426b62bc844cc01b95605e7b4d
Masoud Reyhanian <mrhn2005@gmail.com>
2021-06-05 23:45:30 +0430

    certificates

--- resources/views/components/certificates.blade.php
+++ resources/views/components/certificates.blade.php
@@ -1,11 +1,20 @@
-<div class="row">
-    @foreach ($certificates as $certificate)
-        <div class="col-md-4">
-            <figure class="image-box">
-                <a href="{{Helper::placeholder($certificate->photo)}}" class="fancybox-thumb" rel="fancybox-thumb" title="{{$certificate->getTranslatedAttribute('title')}}">
-                    <img src="{{Helper::placeholder($certificate->photo)}}" alt="{{$certificate->title}}">
-                </a>
-            </figure>
-        </div>
-    @endforeach
+<div class="auto-container">
+    <div class="sec-title-two">
+        <h2>Our Certificates</h2>
+        {{-- <div class="title-text">We deliver to your door</div> --}}
+    </div>
+    <div class="row clearfix">
+        @foreach ($certificates as $certificate)
+            <div class="col-md-3 shop-item">
+                <div class="inner-box">
+                    <div class="image-box">
+                        <a href="{{Helper::placeholder($certificate->photo)}}" class="fancybox-thumb" rel="fancybox-thumb" title="{{$certificate->getTranslatedAttribute('title')}}">
+                            <img src="{{Helper::placeholder($certificate->photo)}}" alt="{{$certificate->title}}">
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        @endforeach
+    </div>
 </div>
--- resources/views/template/pages/about/about.blade.php
+++ resources/views/template/pages/about/about.blade.php
@@ -3,4 +3,6 @@
 
 @section('content')
     <x-welcome/>
+
+    <x-certificates/>
 @endsection
